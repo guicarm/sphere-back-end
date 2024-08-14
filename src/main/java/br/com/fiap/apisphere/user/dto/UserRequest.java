64801dd01a7e64670941f88e70979a965b99cf53
guicarm <rm98874@fiap.com.br>
2024-08-14 08:20:32 -0300

    fix: userRequest

--- src/main/java/br/com/fiap/apisphere/user/dto/UserRequest.java
+++ src/main/java/br/com/fiap/apisphere/user/dto/UserRequest.java
@@ -1,24 +1,22 @@
 package br.com.fiap.apisphere.user.dto;
 
-import br.com.fiap.apisphere.user.User;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import br.com.fiap.apisphere.user.User;
 
 public record UserRequest(
         String name,
         String bio,
         String email,
-        String password
-) {
+        String password) {
     public User toModel() {
-        return new User.builder()
-                 .name(name)
-                 .bio(bio)
-                 .email(email)
-                 .password(password)
-                 .createdAt(LocalDateTime.now())
-                 .updatedAt(LocalDateTime.now())
-                 .build();
+        return User.builder()
+                .name(name)
+                .bio(bio)
+                .email(email)
+                .password(password)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
     }
 }
