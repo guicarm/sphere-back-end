5a61202c99417474671bd4cd1d35dc16d9daf264
guicarm <RM98874@fiap.com.br>
2024-08-27 11:52:19 -0300

    fix: SecurityConfig

--- src/main/java/br/com/fiap/apisphere/auth/SecurityConfig.java
+++ src/main/java/br/com/fiap/apisphere/auth/SecurityConfig.java
@@ -7,23 +7,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain config(HttpSecurity http) throws Exception {
+    public SecurityFilterChain config(HttpSecurity http, AuthorizationFilter authorizationFilter) throws Exception {
         // Filtro Http
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, "/login").permitAll()
                 .requestMatchers(HttpMethod.GET, "/users").permitAll()
                 .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                .requestMatchers(HttpMethod.POST, "/posts").permitAll()
+                .requestMatchers(HttpMethod.POST, "/posts").authenticated()
         );
 
         // Filtro CSRF
         http.csrf(csrf -> csrf.disable());
 
+        http.addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
